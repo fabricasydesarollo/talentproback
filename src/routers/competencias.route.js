461c5faf9b450cdc8764a1d99e2d4db143192d1e
Luis Felipe Cortes <you@example.com>
2024-11-14 16:01:24 +0000

    se agrega ruta

--- src/routers/competencias.route.js
+++ src/routers/competencias.route.js
@@ -1,25 +1,27 @@
 import { Router } from "express";
-import { asignarCompetenciaEvaluacion, asignarCompetenciasCargo, crearCompetencia, crearDescriptor, crearTipoCompetencia, obtenerCompetencia, obtenerDescriptor, obtenerTipoCompetencia } from "../controllers/competencias.controller.js";
+import {
+  asignarCompetenciaEvaluacion,
+  asignarCompetenciasCargo,
+  crearCompetencia,
+  crearDescriptor,
+  crearTipoCompetencia,
+  obtenerCompetencia,
+  obtenerDescriptor,
+  obtenerTipoCompetencia,
+} from "../controllers/competencias.controller.js";
 
-const router = Router()
+const router = Router();
 
-router.route("/")
-    .get(obtenerCompetencia)
-    .post(crearCompetencia)
+router.route("/").get(obtenerCompetencia).post(crearCompetencia);
 
-router.route("/descriptor")
-    .get(obtenerDescriptor)
-    .post(crearDescriptor)
+router.route("/descriptor").get(obtenerDescriptor).post(crearDescriptor);
 
-router.route("/tipo")
-    .get(obtenerTipoCompetencia)
-    .post(crearTipoCompetencia)
+router.route("/tipo").get(obtenerTipoCompetencia).post(crearTipoCompetencia);
 
-router.route("/asignarCompEval")
-    .post(asignarCompetenciaEvaluacion)
+router.route("/asignarCompEval").post(asignarCompetenciaEvaluacion);
 
-router.route("/asignarCompCargo")
-    .post(asignarCompetenciasCargo)
+router.route("/asignarCompCargo").post(asignarCompetenciasCargo);
 
+router.route("/asignarCompCargo").post("");
 
-export default router
+export default router;
